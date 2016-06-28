2b974a98a22196aec7f15ef2eba675043cef88c3
tyang020 <tianruiyang@outlook.com>
2016-06-27 21:02:51 -0700

    [232. Implement Queue using Stacks][Compile Error]committed by LeetCode Extension

---
+++ 232-Implement-Queue-using-Stacks/solution.java
@@ -0,0 +1,22 @@
+class Queue {
+public:
+    // Push element x to the back of queue.
+    void push(int x) {
+        
+    }
+
+    // Removes the element from in front of queue.
+    void pop(void) {
+        
+    }
+
+    // Get the front element.
+    int peek(void) {
+        
+    }
+
+    // Return whether the queue is empty.
+    bool empty(void) {
+        
+    }
+};
