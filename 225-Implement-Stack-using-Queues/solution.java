cb2142a98083700c2a34e9c2d5565791af30e302
tyang020 <tianruiyang@outlook.com>
2016-06-27 21:35:54 -0700

    [225. Implement Stack using Queues][Accepted]committed by LeetCode Extension

--- 225-Implement-Stack-using-Queues/solution.java
+++ 225-Implement-Stack-using-Queues/solution.java
@@ -3,31 +3,31 @@ class MyStack {
     Queue<Integer> q2 = new LinkedList<Integer>();
     // Push element x onto stack.
     public void push(int x) {
-        q1.offer(x);
+        q1.add(x);
     }
 
     // Removes the element on top of the stack.
     public void pop() {
         while(q1.size() > 1)
-            q2.offer(q1.remove());
+            q2.add(q1.remove());
         q1.remove();
         while(!q2.isEmpty())
-            q1.offer(q2.remove());
+            q1.add(q2.remove());
     }
 
     // Get the top element.
     public int top() {
         while(q1.size() > 1)
-            q2.offer(q1.remove());
+            q2.add(q1.remove());
         int top = q1.peek();
-        q2.offer(q1.remove());
+        q2.add(q1.remove());
         while(!q2.isEmpty())
-            q1.offer(q2.remove());
+            q1.add(q2.remove());
         return top;
     }
 
     // Return whether the stack is empty.
     public boolean empty() {
-        return q1.isEmpty && q2.isEmpty();
+        return q1.isEmpty() && q2.isEmpty();
     }
 }
