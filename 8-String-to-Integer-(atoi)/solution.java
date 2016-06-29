56b92dfacf998b97e066131c215198a09215f710
tyang020 <tianruiyang@outlook.com>
2016-06-29 16:44:44 -0700

    [8. String to Integer (atoi)][Compile Error]committed by LeetCode Extension

--- 8-String-to-Integer-(atoi)/solution.java
+++ 8-String-to-Integer-(atoi)/solution.java
@@ -1,30 +1,36 @@
 public class Solution {
     public int myAtoi(String str) {
-       if(str == null) return 0;
-       int i = 0;
-       long res = 0;
-       char sign = '.';
-       while (i < str.length() && !isNumber(str.charAt(i))) {
-           sign = isSign(str.charAt(str.charAt(i))) ? str.charAt(i) : '.';
+        if(str == null || str.length() < 1) return 0;
+        int i = 0;
+        long res = 0;
+        char sign = '+';
+        //remove all whitespace
+        str = str.trim();
+        /*       
+        while (i < str.length() && !isNumber(str.charAt(i))) {
+           sign = isSign(str.charAt(i)) ? str.charAt(i) : '.';
            i++;
-       }
-       while (i < str.length() && isNumber(str.charAt(i))) {
-           res = res * 10 + str.charAt(i) - '0';
-           i++;
-       }
-       if (sign == '-') 
+        }
+        */
+        if(str.charAt(0) == '-'){
+            sign = '-';
+            i++;
+        } else if (str.charAt(0) == '+') 
+            i++;
+        while (i < str.length() && isNumber(str.charAt(i))) {
+            res = res * 10 + (str.charAt(i) - '0');
+            i++;
+        }
+        if (sign == '-') 
             res = -res;
-       if (res < Integer.MIN_VALUE) {
-           return Integer.MIN_VALUE;
-       } else if (res > Integer.MAX_VALUE) {
+        if (res < Integer.MIN_VALUE) {
+            return Integer.MIN_VALUE;
+        } else if (res > Integer.MAX_VALUE) {
            return Integer.MAX_VALUE;
-       } else
+        } else
             return (int)res;
     }
     private static boolean isNumber(char c) {
-       return c > '0' && c < '9';
-    }
-    private static boolean isSign(char c) {
-        return c == '+' || c == '-';
+        return c >= '0' && c =< '9';
     }
 }
