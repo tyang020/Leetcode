5c2de382a0c7514e09b1e4bbc439b44f6eb931bd
tyang020 <tianruiyang@outlook.com>
2016-06-28 18:58:37 -0700

    [26. Remove Duplicates from Sorted Array][Compile Error]committed by LeetCode Extension

---
+++ 26-Remove-Duplicates-from-Sorted-Array/solution_v2.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public int removeDuplicates(int[] nums) {
+        int i = 0;
+        //n: original array
+        //nums[i]: new array without duplicates
+        for(int n : nums) {
+            if (i < 1 || n > nums[i - 1]) {
+                num[i] = n;
+            }
+        }
+        return i;
+    }
+}
