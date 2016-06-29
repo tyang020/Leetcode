70d53ac43c2436730ad354fab11a407b83e234d1
tyang020 <tianruiyang@outlook.com>
2016-06-29 16:46:37 -0700

    [8. String to Integer (atoi)][Accepted]committed by LeetCode Extension

--- 8-String-to-Integer-(atoi)/solution.java
+++ 8-String-to-Integer-(atoi)/solution.java
@@ -2,7 +2,7 @@ public class Solution {
     public int myAtoi(String str) {
         if(str == null || str.length() < 1) return 0;
         int i = 0;
-        long res = 0;
+        double res = 0;
         char sign = '+';
         //remove all whitespace
         str = str.trim();
@@ -17,12 +17,15 @@ public class Solution {
             i++;
         } else if (str.charAt(0) == '+') 
             i++;
+            
         while (i < str.length() && isNumber(str.charAt(i))) {
             res = res * 10 + (str.charAt(i) - '0');
             i++;
         }
+        
         if (sign == '-') 
             res = -res;
+            
         if (res < Integer.MIN_VALUE) {
             return Integer.MIN_VALUE;
         } else if (res > Integer.MAX_VALUE) {
@@ -30,7 +33,8 @@ public class Solution {
         } else
             return (int)res;
     }
+    
     private static boolean isNumber(char c) {
-        return c >= '0' && c =< '9';
+        return c >= '0' && c <= '9';
     }
 }
