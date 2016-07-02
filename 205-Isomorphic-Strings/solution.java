e783e922d52da83bda49311d84d56a00e23ce94d
tyang020 <tianruiyang@outlook.com>
2016-07-01 22:30:10 -0700

    [205. Isomorphic Strings][Runtime Error]committed by LeetCode Extension

--- 205-Isomorphic-Strings/solution.java
+++ 205-Isomorphic-Strings/solution.java
@@ -1,10 +1,11 @@
 public class Solution {
     public boolean isIsomorphic(String s, String t) {
-        int[] count = new int[26];
+        //acsii code 0-255
+        int[] count = new int[256];
         for (int i = 0; i < s.length(); i++) {
-            if (count[s.charAt(i)-'a'] == 0) {
-                count[s.charAt(i) - 'a'] = t.charAt(i) - 'a' + 1;
-            } else if (count[s.charAt(i) - 'a'] != t.charAt(i) - 'a' + 1)
+            if (count[s.charAt(i)] == 0) {
+                count[s.charAt(i)] = t.charAt(i) + 1;
+            } else if (count[s.charAt(i) - 'a'] != t.charAt(i) + 1)
                 return false;
         }
         return true;
