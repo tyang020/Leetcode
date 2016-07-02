e700529698571fb08a4cfc91c03b864a97c31884
tyang020 <tianruiyang@outlook.com>
2016-07-01 23:07:34 -0700

    [290. Word Pattern][Runtime Error]committed by LeetCode Extension

---
+++ 290-Word-Pattern/solution.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public boolean wordPattern(String pattern, String str) {
+        String[] word = str.split(" ");
+        Map<Character, String> map = new HashMap();
+        Set<String> set = new HashSet();
+        for (int i = 0; i < pattern.length(); i++) {
+            if (!map.containsKey(pattern.charAt(i))) {
+                if (!set.add(word[i])) return false;
+                map.put(pattern.charAt(i),word[i]);
+            } else if (!map.get(pattern.charAt(i)).equals(word[i]))
+                return false;
+        }
+        return true;
+    }
+}
