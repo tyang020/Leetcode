843e59e5c00d8badd9a9022e19c4d16426fcec71
tyang020 <tianruiyang@outlook.com>
2016-07-03 11:20:05 -0700

    [58. Length of Last Word][Accepted]committed by LeetCode Extension

--- 58-Length-of-Last-Word/solution_v1.java
+++ 58-Length-of-Last-Word/solution_v1.java
@@ -4,8 +4,9 @@ public class Solution {
         while (i >= 0 && s.charAt(i) == ' ') {
             i--;
         }
-        while (i-- >= 0 && s.charAt(i) != ' ') {
+        while (i >= 0 && s.charAt(i) != ' ') {
             len++;
+            i--;
         }
         return len;
     }
