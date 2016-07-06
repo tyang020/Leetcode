d5e0377686e61f4b10b2eff7c102e904f811079d
tyang020 <tianruiyang@outlook.com>
2016-07-06 11:40:42 -0700

    [1. Two Sum][Accepted]committed by LeetCode Extension

---
+++ 1-Two-Sum/solution.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] + nums[i] == target) {
+                    int[] res = {i, j};
+                    return res;
+                }
+            }
+        }
+        return null;
+    }
+}
