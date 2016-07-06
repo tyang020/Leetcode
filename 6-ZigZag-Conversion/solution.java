f8a4138d31ceec72c7872d85e1e431cdefba35e1
tyang020 <tianruiyang@outlook.com>
2016-07-06 12:36:45 -0700

    [6. ZigZag Conversion][Runtime Error]committed by LeetCode Extension

---
+++ 6-ZigZag-Conversion/solution.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public String convert(String s, int numRows) {
+        if (s.length() == 0 || numRows == 0) return "";
+        StringBuilder[] sbs = new StringBuilder[numRows];
+        for (int i = 0; i < s.length(); i++) {
+            int mod = i % (numRows * 2 - 2);
+            if (mod < numRows) 
+                sbs[mod].append(s.charAt(i));
+            else
+                sbs[numRows - 2 - mod%numRows].append(s.charAt(i));
+        }
+        for (int i = 1; i < numRows; i++) {
+            sbs[0].append(sbs[i]);
+        }
+        return sbs[0].toString();
+    }
+}
