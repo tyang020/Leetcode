a7b614b7509106a4ca51e041112cea3ab77d7a85
tyang020 <tianruiyang@outlook.com>
2016-07-07 11:14:31 -0700

    [338. Counting Bits][Compile Error]committed by LeetCode Extension

---
+++ 338-Counting-Bits/solution_v2.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public int[] countBits(int num) {
+        /** The number of 1s in an integer n is the equal to 1 + res(cut off the most signigicant bit of n) */
+        int p = 1, pow = 1;
+        //pow : keeps track of the number of 2's power, while has only one 1.
+        //p : keeps track of the difference between the current number and its corresponding power, i.e. 1100 p = 100
+        for (int i = 1; i <= num; i++) {
+            if (i == pow) {
+                res[i] = 1;
+                pow << = 1;
+                p = 1;
+            } else {
+                res[i] = res[p] + 1; //res(1100) = res(100) + 1
+                p++;
+            }
+        }
+    }
+}
