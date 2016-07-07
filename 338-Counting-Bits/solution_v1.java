2fdded4a6eb11b108cf810a795de94afa8f15e01
tyang020 <tianruiyang@outlook.com>
2016-07-07 10:46:22 -0700

    [338. Counting Bits][Accepted]committed by LeetCode Extension

--- 338-Counting-Bits/solution_v1.java
+++ 338-Counting-Bits/solution_v1.java
@@ -1,7 +1,7 @@
 public class Solution {
     public int[] countBits(int num) {
         /** The difference between i(1110) and i/2(111) is only whether the remaining bit is 0 or 1. res[i] = res[i/2] + i%2*/
-        int[] res = new res[num+1];
+        int[] res = new int[num+1];
         for (int i = 0; i <= num; i++) {
             res[i] = res[i >> 1] + (i & 1);
         }
