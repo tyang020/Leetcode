f4883f45dc9d9dbbcd11fa981528a2d44e79ee7e
tyang020 <tianruiyang@outlook.com>
2016-07-12 11:55:15 -0700

    [343. Integer Break][Accepted]committed by LeetCode Extension

--- 343-Integer-Break/solution.java
+++ 343-Integer-Break/solution.java
@@ -1,5 +1,7 @@
 public class Solution {
     public int integerBreak(int n) {
+        //O(n) time and O(n) space
+        /*
         int[] res = new int[n+1];
         for (int i = 2; i <= n; i++) {
             if (i < 7) {
@@ -8,5 +10,16 @@ public class Solution {
                 res[i] = 3 * res[i - 3];
         }
         return res[n];
+        */
+        
+        //O(n) time and O(1) space
+        if (n == 2) return 1;
+        if (n == 3) return 2;
+        int res = 1;
+        while (n > 4) {
+            res *= 3;
+            n -= 3;
+        }
+        return n * res;
     }
 }
