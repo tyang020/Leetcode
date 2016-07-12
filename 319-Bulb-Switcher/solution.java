8eefa6e578c6cdc454fce99a91ac48985377c456
tyang020 <tianruiyang@outlook.com>
2016-07-12 15:39:49 -0700

    [319. Bulb Switcher][Accepted]committed by LeetCode Extension

---
+++ 319-Bulb-Switcher/solution.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public int bulbSwitch(int n) {
+        if (n == 0) return 0;
+        int i;
+        for (i = 1; i * i <= n; i++) {
+            
+        }
+        return i-1;
+    }
+}
