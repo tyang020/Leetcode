0d37befaec7614211768c4b0d221df965e3dd1d4
tyang020 <tianruiyang@outlook.com>
2016-07-12 15:46:47 -0700

    [319. Bulb Switcher][Accepted]committed by LeetCode Extension

--- 319-Bulb-Switcher/solution.java
+++ 319-Bulb-Switcher/solution.java
@@ -1,5 +1,10 @@
 public class Solution {
     public int bulbSwitch(int n) {
+        /** If a number i is a square number, it has odd number of factors, then after n round the ith bulb is on. 
+         *  eg 9 has factors 1,3,9(on,off,on)
+         *  
+         *  Else it has even number of factors, i.e., 7 has 1 and 7, and 8 has 1,2,4,8, then the ith bulb is off after n round.
+         */
         if (n == 0) return 0;
         int i;
         for (i = 1; i * i <= n; i++) {
