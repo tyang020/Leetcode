845e044a8d48ad9d6635401e71cc6e7441b4864e
tyang020 <tianruiyang@outlook.com>
2016-07-17 16:29:33 -0700

    [108. Convert Sorted Array to Binary Search Tree][Runtime Error]committed by LeetCode Extension

---
+++ 108-Convert-Sorted-Array-to-Binary-Search-Tree/solution.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        if (nums.length == 0) return null;
+        TreeNode root = new TreeNode(0);
+        helper(root, nums, 0, nums.length - 1);
+        return root;
+    }
+    public void helper (TreeNode root, int[] nums, int start, int end) {
+        if (start == end) return;
+        int mid = start + (end - start) / 2;
+        root.val = nums[mid];
+        helper(root.left, nums, start, mid);
+        helper(root.right, nums, mid+1, end);
+    }
+}
