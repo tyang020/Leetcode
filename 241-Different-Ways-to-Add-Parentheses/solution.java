57e5bc72e128326239c94b84d07d57edc7df9a1c
tyang020 <tianruiyang@outlook.com>
2016-07-19 13:23:14 -0700

    [241. Different Ways to Add Parentheses][Accepted]committed by LeetCode Extension

--- 241-Different-Ways-to-Add-Parentheses/solution.java
+++ 241-Different-Ways-to-Add-Parentheses/solution.java
@@ -1,10 +1,13 @@
 public class Solution {
     /** The idea is similar to unique BST: the operator will divide the input to 2 parts and compute those seperately.
-     *  Without map: 9ms; with map
+     *  Without map: 9ms; with map 11ms;
      */
     private Map<String, List<Integer>> map = new HashMap();
     
     public List<Integer> diffWaysToCompute(String input) {
+        if (map.containsKey(input))
+            return map.get(input);
+            
         List<Integer> list = new ArrayList();
         
         for (int i = 0; i < input.length(); i++) {
@@ -14,11 +17,11 @@ public class Solution {
             String s1 = input.substring(0, i);
             String s2 = input.substring(i + 1);
             // Without map:
-            // List<Integer> n1 = diffWaysToCompute(s1);
-            // List<Integer> n2 = diffWaysToCompute(s2);
+            List<Integer> n1 = diffWaysToCompute(s1);
+            List<Integer> n2 = diffWaysToCompute(s2);
             
-            List<Integer> n1 = map.getOrDefault(s1, diffWaysToCompute(s1));
-            List<Integer> n2 = map.getOrDefault(s2, diffWaysToCompute(s2));
+            //List<Integer> n1 = map.getOrDefault(s1, diffWaysToCompute(s1));
+            //List<Integer> n2 = map.getOrDefault(s2, diffWaysToCompute(s2));
             for (Integer c1 : n1) {
                 for (Integer c2 : n2) {
                     switch(c) {
