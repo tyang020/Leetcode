8f151c825d0afe2e70af1adceae159848a895eb0
tyang020 <tianruiyang@outlook.com>
2016-07-18 21:26:49 -0700

    [96. Unique Binary Search Trees][Accepted]committed by LeetCode Extension

--- 96-Unique-Binary-Search-Trees/solution.java
+++ 96-Unique-Binary-Search-Trees/solution.java
@@ -5,12 +5,12 @@ public class Solution {
         if (n == 0) return 1;
         for (int i = 1; i < n+1; i++) {
             int j;
-            for (j = 0; j < i / 2; j++) {
-                f[i] += 2 * f[i - 1 - j] * f[j];
+            for (j = 0; j < i; j++) {
+                f[i] += f[i - 1 - j] * f[j];
             }
-            if (i % 2 != 0) {
+          /*  if (i % 2 != 0) {
                 f[i] += f[j] * f[j];
-            }
+            }*/
         }
         return f[n];
     }
