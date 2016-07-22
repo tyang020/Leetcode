0c1b5797f2c0967a797ccf21eb8cd5cf5b46a4d4
tyang020 <tianruiyang@outlook.com>
2016-07-21 22:09:03 -0700

    [376. Wiggle Subsequence][Compile Error]committed by LeetCode Extension

---
+++ 376-Wiggle-Subsequence/solution.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public int wiggleMaxLength(int[] nums) {
+        if (nums.length < 2) return nums.length;
+        int diff = nums[1] - nums[0], count = 2;
+        int i = 1;
+        while (diff == 0) {
+            i++;
+            count = 1;
+        }
+        for (i ; i < nums.length; i++) {
+            if (diff == 0) continue;
+            if ((nums[i] - nums[i-1]) * diff < 0) {
+                count++;
+                diff = -diff;
+            }
+        }
+        return count;
+    }
+}
