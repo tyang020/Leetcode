60dd94f3f35dc6526478c6fa0a80c75a41082def
tyang020 <tianruiyang@outlook.com>
2016-07-31 16:33:22 -0700

    [75. Sort Colors][Accepted]committed by LeetCode Extension

--- 75-Sort-Colors/solution.java
+++ 75-Sort-Colors/solution.java
@@ -1,11 +1,10 @@
 public class Solution {
     public void sortColors(int[] nums) {
         int second = nums.length - 1, zero = 0;
-        for (int i = 0; i < second; i++) {
+        for (int i = 0; i <= second; i++) {
             if (nums[i] == 0) {
                 nums[i] = nums[zero];
-                nums[zero] = 0;
-                zero
+                nums[zero++] = 0;
             }
             else if (nums[i] == 2) {
                 nums[i--] = nums[second];
