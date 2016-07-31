e1c2088bbcd23721bd4a4cdb6a1bc00df13df945
tyang020 <tianruiyang@outlook.com>
2016-07-30 17:37:39 -0700

    [334. Increasing Triplet Subsequence][Compile Error]committed by LeetCode Extension

---
+++ 334-Increasing-Triplet-Subsequence/solution.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        // n1 stores the smallest value so far, n2 stores the second value of the sequence
+        int n1 = Math.MAX_VALUE, n2 = Math.MAX_VALUE;
+        for (int n : nums) {
+            if (n < n1)
+                n1 = n;
+            else if (n < n2) {
+                n2 = n;
+            }
+            else
+                return true;
+        }
+        return false;
+    }
+}
