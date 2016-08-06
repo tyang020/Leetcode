044b1717e230d457d1dc3bb27b03fb0b79a6c24c
tyang020 <tianruiyang@outlook.com>
2016-08-06 14:58:48 -0700

    [341. Flatten Nested List Iterator][Runtime Error]committed by LeetCode Extension

---
+++ 341-Flatten-Nested-List-Iterator/solution.java
@@ -0,0 +1,50 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * public interface NestedInteger {
+ *
+ *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     public boolean isInteger();
+ *
+ *     // @return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // Return null if this NestedInteger holds a nested list
+ *     public Integer getInteger();
+ *
+ *     // @return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // Return null if this NestedInteger holds a single integer
+ *     public List<NestedInteger> getList();
+ * }
+ */
+public class NestedIterator implements Iterator<Integer> {
+    Stack<NestedInteger> stack = new Stack<NestedInteger>();
+
+    public NestedIterator(List<NestedInteger> nestedList) {
+        pushStack(nestedList);
+    }
+    
+    private void pushStack(List<NestedInteger> nList) {
+        for (int i = nList.size()-1; i >= 0; i--) 
+            stack.push(nList.get(i));
+    }
+
+    @Override
+    public Integer next() {
+        while (true) {
+            NestedInteger curr = stack.pop();
+            if (curr.isInteger())
+                return curr.getInteger();
+            pushStack(curr.getList());
+        }
+    }
+
+    @Override
+    public boolean hasNext() {
+        return !stack.isEmpty();
+    }
+}
+
+/**
+ * Your NestedIterator object will be instantiated and called as such:
+ * NestedIterator i = new NestedIterator(nestedList);
+ * while (i.hasNext()) v[f()] = i.next();
+ */
