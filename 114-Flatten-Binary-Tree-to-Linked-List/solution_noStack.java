f6657533ed6984e1ea40d875f85225974b3af20e
tyang020 <tianruiyang@outlook.com>
2016-08-08 22:03:45 -0700

    [114. Flatten Binary Tree to Linked List][Time Limit Exceeded]committed by LeetCode Extension

---
+++ 114-Flatten-Binary-Tree-to-Linked-List/solution_noStack.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public void flatten(TreeNode root) {
+        TreeNode curr = root;
+        while (curr != null) {
+            if (curr.left != null) {
+                TreeNode lastRight = curr.left;
+                // Find the last right node, which is the end of flattened left tree;
+                while (lastRight.right != null) 
+                    lastRight = lastRight.right;
+                lastRight.right = curr.right;
+                curr.right = curr.left;
+                curr.left = null;
+            }
+        }
+    }
+}
