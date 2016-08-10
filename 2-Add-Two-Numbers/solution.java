3d727f94d8788b118dad7eb2fd3e9cfb9d659eae
tyang020 <tianruiyang@outlook.com>
2016-08-10 00:12:02 -0700

    [2. Add Two Numbers][Accepted]committed by LeetCode Extension

--- 2-Add-Two-Numbers/solution.java
+++ 2-Add-Two-Numbers/solution.java
@@ -8,21 +8,23 @@
  */
 public class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        int carry = 0;
+        int sum = 0;
         ListNode dummy = new ListNode(0), p = dummy;
         while (l1 != null || l2 != null) {
-            int sum = (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val) + carry;
-            carry = sum / 10;
+            sum /= 10; // carryout from last bit;
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
             p.next = new ListNode(sum % 10);
             p = p.next;
-            
-            if (l1 != null)
-                l1 = l1.next;
-            if (l2 != null)
-                l2 = l2.next;
         }
-        if (carry != 0)
-            p.next = new ListNode(carry);
+        if (sum / 10 == 1)
+            p.next = new ListNode(1);
         return dummy.next;
     }
 }
