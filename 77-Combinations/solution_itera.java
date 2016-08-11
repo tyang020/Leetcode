ff88a6f6549c0b325ed83ec531c1e50fa819ce7c
tyang020 <tianruiyang@outlook.com>
2016-08-11 16:41:47 -0700

    [77. Combinations][Accepted]committed by LeetCode Extension

--- 77-Combinations/solution_itera.java
+++ 77-Combinations/solution_itera.java
@@ -2,13 +2,13 @@ public class Solution {
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> res = new ArrayList();
         
-        for (int i = 1; i <= n; i++) res.add(Arrays.asList(i));
+        for (int i = 1; i <= n-k+1; i++) res.add(Arrays.asList(i));
         
         // In iteration i, the length of each element in res is going to be i;
         for(int i = 2; i <= k; i++) {
             List<List<Integer>> new_res = new ArrayList();
             for (List<Integer> l : res) {
-                for (int j = l.get(l.size()-1)+1; j <= n; j++) {
+                for (int j = l.get(l.size()-1)+1; j <= n-(k-i); j++) {
                     List<Integer> newl = new ArrayList(l);
                     newl.add(j);
                     new_res.add(newl);
