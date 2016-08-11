22daf24772e4964a65fb604004a42e6dbdc07af7
tyang020 <tianruiyang@outlook.com>
2016-08-11 16:35:13 -0700

    [77. Combinations][Time Limit Exceeded]committed by LeetCode Extension

--- 77-Combinations/solution_1.java
+++ 77-Combinations/solution_1.java
@@ -1,15 +1,21 @@
 public class Solution {
     public List<List<Integer>> combine(int n, int k) {
-        if (k == n || k == 0) {
-            List<Integer> row = new LinkedList<>();
-            for (int i = 1; i <= k; ++i) {
-                row.add(i);
+        if (k == 0 || n == 0 || k > n) return Collections.emptyList();
+        List<List<Integer>> combs = new ArrayList<>();
+        for (int i = 1; i <= n; i++) combs.add(Arrays.asList(i));
+        for (int i = 2; i <= k; i++) {
+            List<List<Integer>> newCombs = new ArrayList<>();
+            for (int j = i; j <= n; j++) {
+                for (List<Integer> comb : combs) {
+                    if (comb.get(comb.size()-1) < j) {
+                        List<Integer> newComb = new ArrayList<>(comb);
+                        newComb.add(j);
+                        newCombs.add(newComb);
+                    }
+                }
             }
-            return new LinkedList<>(Arrays.asList(row));
+            combs = newCombs;
         }
-        List<List<Integer>> result = this.combine(n - 1, k - 1);
-        result.forEach(e -> e.add(n));
-        result.addAll(this.combine(n - 1, k));
-        return result;
+        return combs;
     }
 }
