111402f3bb138e2a831d11b638ef97976fd23612
tyang020 <tianruiyang@outlook.com>
2016-08-16 15:45:47 -0700

    [60. Permutation Sequence][Accepted]committed by LeetCode Extension

--- 60-Permutation-Sequence/solution.java
+++ 60-Permutation-Sequence/solution.java
@@ -1,29 +1,34 @@
 public class Solution {
     public String getPermutation(int n, int k) {
+        List<Integer> numbers = new ArrayList<>();
+        int[] factorial = new int[n+1];
         StringBuilder sb = new StringBuilder();
         
-        int[] fact = new int[n+1];
-        fact[0] = 1;
+        // create an array of factorial lookup
+        int sum = 1;
+        factorial[0] = 1;
+        for(int i=1; i<=n; i++){
+            sum *= i;
+            factorial[i] = sum;
+        }
+        // factorial[] = {1, 1, 2, 6, 24, ... n!}
+        
+        // create a list of numbers to get indices
+        for(int i=1; i<=n; i++){
+            numbers.add(i);
+        }
+        // numbers = {1, 2, 3, 4}
+        
         k--;
-        for (int i = 1; i <= n; i++) {
-            fact[i] = fact[i-1] * i;
+        
+        for(int i = 1; i <= n; i++){
+            int index = k/factorial[n-i];
+            sb.append(String.valueOf(numbers.get(index)));
+            numbers.remove(index);
+            k-=index*factorial[n-i];
         }
         
-        
-        List<Integer> list = new ArrayList<Integer>();
-        for (int j = 1; j <= n; j++) 
-            list.add(j);
-        
-        
-        for (int i = 1; i <= n; i++) {
-            int idx = k / fact[n-i];
-            sb.append(list.get(idx));
-            list.remove(idx);
-            // next k
-            k %= fact[n-i];
-        }
-        
-        return sb.toString();
+        return String.valueOf(sb);
     }
     
 }
