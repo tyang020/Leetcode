26c7e76919c3742e2321bb5d73aaad278d41e495
tyang020 <tianruiyang@outlook.com>
2016-08-16 15:22:29 -0700

    [60. Permutation Sequence][Time Limit Exceeded]committed by LeetCode Extension

--- 60-Permutation-Sequence/solution.java
+++ 60-Permutation-Sequence/solution.java
@@ -15,15 +15,16 @@ public class Solution {
             list.add(j);
         
         // Find the largest factorial which is smaller than k
+        factorial /= i--;
         
-        
-        while (i != 0) {
-            factorial /= i--;
+        while (i > 0) {
             int idx = k / factorial;
             sb.append(list.get(idx));
             list.remove(idx);
             k %= factorial;
+            factorial /= i--;
         }
+        sb.append(list.get(0));
         
         return sb.toString();
     }
