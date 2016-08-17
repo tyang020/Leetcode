80395b9e432dcb7fa8a0b42603d758fbda860db7
tyang020 <tianruiyang@outlook.com>
2016-08-16 21:18:28 -0700

    [148. Sort List][Runtime Error]committed by LeetCode Extension

---
+++ 148-Sort-List/solution.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode sortList(ListNode head) {
+        // partition the original list into two parts, find the middle node
+        if (head == null || head.next == null)
+            return head;
+        ListNode slow = head, fast = head.next.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode left = sortList(head);
+        ListNode right = sortList(slow.next);
+        slow.next = null;
+        return merge(left, right);
+    }
+    
+    private ListNode merge(ListNode left, ListNode right) {
+        ListNode dummy = new ListNode(0), p = dummy;
+        while (left != null && right != null) {
+            if (left.val < right.val) {
+                p.next = left;
+                left = left.next;
+            } else {
+                p.next = right;
+                right = right.next;
+            }
+            p = p.next;
+        }
+        if (left == null)
+            p.next = right;
+        if (right == null) 
+            p.next = left;
+        return dummy.next;
+    }
+}
