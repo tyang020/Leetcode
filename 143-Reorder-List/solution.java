12298a19aa5eaf2c08daf8f0953f6913734d7139
tyang020 <tianruiyang@outlook.com>
2016-08-17 00:16:37 -0700

    [143. Reorder List][Time Limit Exceeded]committed by LeetCode Extension

---
+++ 143-Reorder-List/solution.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public void reorderList(ListNode head) {
+        // 1->2->3->4->5->6
+        if (head == null || head.next == null)
+            return;
+            
+        ListNode slow = head, fast = head;
+        // find middle node and reverse nodes after middle
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        } //slow = 3
+        
+        // prev refer to the first node of reversed list
+        ListNode prev = null, cur = slow.next;
+        while(cur != null) {
+            ListNode next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        slow = head;
+        fast = prev;
+        while (fast != null) {
+            ListNode slow_next = slow.next;
+            slow.next = fast;
+            slow.next.next = slow_next;
+            slow = slow_next;
+            fast = fast.next;
+        }
+        slow.next = fast;
+        
+    }
+}
