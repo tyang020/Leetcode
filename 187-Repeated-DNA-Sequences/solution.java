dc739d3857ff009f97fdde9913ad24b040086896
tyang020 <tianruiyang@outlook.com>
2016-08-18 22:56:09 -0700

    [187. Repeated DNA Sequences][Runtime Error]committed by LeetCode Extension

---
+++ 187-Repeated-DNA-Sequences/solution.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public List<String> findRepeatedDnaSequences(String s) {
+        // Ascii code of A: 0x
+        Set<Integer> set = new HashSet();
+        Set<String> res = new HashSet();
+        int seq = 0;   // each letter uses 3 bit of seq and total is 30 bit which is smaller than 32
+        for (int i = 0; i < 9; i++)
+            seq = (seq << 3) | (s.charAt(i) & 7);
+        for (int i = 9; i < s.length(); i++) {
+            seq = (seq << 3 & 0x3FFFFFFF) | (s.charAt(i) & 7);
+            if (!set.add(seq))
+                res.add(s.substring(i-9, i+1));
+        }
+        return new ArrayList(res);
+    }
+}
