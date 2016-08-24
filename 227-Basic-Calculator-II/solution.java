453cf80efbd3794aabb9fda4f6d4e02497180125
tyang020 <tianruiyang@outlook.com>
2016-08-23 23:32:41 -0700

    [227. Basic Calculator II][Runtime Error]committed by LeetCode Extension

---
+++ 227-Basic-Calculator-II/solution.java
@@ -0,0 +1,36 @@
+public class Solution {
+    public int calculate(String s) {
+        int len = s.length();
+        int res = 0;
+        long sub = 0;
+        char sign = '+';
+        int i = 0;
+        while (i < len) {
+            long num = 0;
+            while (i < len && Character.isDigit(s.charAt(i))) {
+                num = num*10 + s.charAt(i) - '0';
+                i++;
+            }
+            // When encounter '+' or '-' add sub to res;
+            // Else calculate sub;
+            if (sign == '+') {
+                res += sub;
+                sub = num;
+            } else if (sign == '-') {
+                res += sub;
+                sub = -num;
+            } else if (sign == '*') {
+                sub *= num;
+            } else if (sign == '/') {
+                sub /= num;
+            }
+            if (i < len) {
+                if (s.charAt(i) != ' ')
+                    sign = s.charAt(i);
+                i++;
+            }
+        }
+        res += sub;
+        return res;
+    }
+}
