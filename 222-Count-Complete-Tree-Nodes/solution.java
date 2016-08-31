ddc0264ef4e7ceb26ac4fe1e5ceba31813adda09
tyang020 <tianruiyang@outlook.com>
2016-08-30 17:47:18 -0700

    [222. Count Complete Tree Nodes][Runtime Error]committed by LeetCode Extension

---
+++ 222-Count-Complete-Tree-Nodes/solution.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int countNodes(TreeNode root) {
+        TreeNode p = root;
+        int h = height(root), count = 0;
+        while (p != null) {
+            if (height(p.right) == h - 1) {
+                count += 1 << h;   // the number of nodes of left tree plus one root
+                p = p.right;
+            } else {
+                count += 1 << (h-1); //# of node in right tree plus one root
+                p = p.left;
+            }
+        }
+        return count;
+    }
+    
+    private int height(TreeNode root) {
+        int h = -1;
+        TreeNode p = root;
+        while (p != null) {
+            h++;
+            root = root.left;
+        }
+        return h;
+    }
+}
