1786f3bc708e881c4568651c2ed1a6d27aa7c9c3
tyang020 <tianruiyang@outlook.com>
2016-08-31 15:52:30 -0700

    [91. Decode Ways][Accepted]committed by LeetCode Extension

---
+++ 91-Decode-Ways/solution.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public int numDecodings(String s) {
+        int len = s.length();
+        if (len == 0) return 0;
+        int[] dp = new int[len+1];
+        dp[len] = 1;
+        dp[len-1] = s.charAt(len-1) == '0' ? 0 : 1;
+        for (int i = len-2; i >= 0; i--) {
+            if (s.charAt(i) == '0')
+                continue;
+            if (Integer.valueOf(s.substring(i,i+2)) <= 26)
+                dp[i] = dp[i+1] + dp[i+2];
+            else
+                dp[i] = dp[i+1];
+        }
+        return dp[0];
+    }
+}
