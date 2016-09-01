9517a7e71f2fd596ba71aa605d69b4a7c72fa6f5
tyang020 <tianruiyang@outlook.com>
2016-08-31 23:46:35 -0700

    [71. Simplify Path][Accepted]committed by LeetCode Extension

--- 71-Simplify-Path/solution_v2.java
+++ 71-Simplify-Path/solution_v2.java
@@ -2,7 +2,7 @@ public class Solution {
     public String simplifyPath(String path) {
         Set<String> skip = new HashSet(Arrays.asList("", ".", ".."));
         Stack<String> stack = new Stack();
-        for (String dir : path.split("/+")) {
+        for (String dir : path.split("/+")) { // one or more slash represented by regexp
             if (dir.equals("..") && !stack.isEmpty())
                 stack.pop();
             else if(!skip.contains(dir))
