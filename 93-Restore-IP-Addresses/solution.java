ed1a3b16849bed25b0797feb50534cd7510ceeec
tyang020 <tianruiyang@outlook.com>
2016-08-31 22:36:50 -0700

    [93. Restore IP Addresses][Runtime Error]committed by LeetCode Extension

--- 93-Restore-IP-Addresses/solution.java
+++ 93-Restore-IP-Addresses/solution.java
@@ -3,13 +3,15 @@ public class Solution {
     public List<String> restoreIpAddresses(String s) {
         List<String> res = new ArrayList();
         int len = s.length();
-        for (int i = len/4; i <= 3 && i < len-2; i++) {
+        if (len < 4)
+            return res;
+        for (int i = 1; i <= 3 && i < len-2; i++) {
             String s1 = s.substring(0, i);
             if (!isValid(s1)) break;
-            for (int j = i + (len-i)/3; j - i <= 3 && j < len - 1; j++) {
+            for (int j = i + 1; j - i <= 3 && j < len - 1; j++) {
                 String s2 = s.substring(i,j);
                 if (!isValid(s2)) break;
-                for (int k = j + (len-j)/2; k - j <= 3 && k < len; k++) {
+                for (int k = j + 1; k - j <= 3 && k < len; k++) {
                     String s3 = s.substring(j, k), s4 = s.substring(k);
                     if (!isValid(s3)) break;
                     if (!isValid(s4)) continue;
