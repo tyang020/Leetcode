aafd051ecc7c3eefc4a315725af0c0830b63341b
tyang020 <tianruiyang@outlook.com>
2016-09-01 14:45:45 -0700

    [133. Clone Graph][Accepted]committed by LeetCode Extension

--- 133-Clone-Graph/solution.java
+++ 133-Clone-Graph/solution.java
@@ -10,10 +10,10 @@ public class Solution {
     Map<Integer, UndirectedGraphNode> map = new HashMap<>();   // map stores node lables which are cloned and their cloned nodes.
     public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
         if (node == null) return null;
-        if (map.containsKey(node.lable))
-            return map.get(node.lable);
-        UndirectedGraphNode cloned = new UndirectedGraphNode(node.lable);  // clone lable
-        map.put(node.lable, cloned);
+        if (map.containsKey(node.label))
+            return map.get(node.label);
+        UndirectedGraphNode cloned = new UndirectedGraphNode(node.label);  // clone label
+        map.put(node.label, cloned);
         for (UndirectedGraphNode neighbor: node.neighbors) {
             cloned.neighbors.add(cloneGraph(neighbor));  // add each cloned neighbor to clone.neighbors
         }
