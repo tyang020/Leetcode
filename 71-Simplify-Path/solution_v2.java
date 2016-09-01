bfeb4a4370171dec738a779d3d714aa846f8ebbf
tyang020 <tianruiyang@outlook.com>
2016-08-31 23:41:17 -0700

    [71. Simplify Path][Accepted]committed by LeetCode Extension

---
+++ 71-Simplify-Path/solution_v2.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public String simplifyPath(String path) {
+        Set<String> skip = new HashSet(Arrays.asList("", ".", ".."));
+        Stack<String> stack = new Stack();
+        for (String dir : path.split("/+")) {
+            if (dir.equals("..") && !stack.isEmpty())
+                stack.pop();
+            else if(!skip.contains(dir))
+                stack.push(dir);
+        }
+        String res = "";
+        while (!stack.isEmpty()) res = "/" + stack.pop() + res;
+        return res == "" ? "/" : res;
+    }
+}
